2bd5518f0ddafe23375ad36b13d1834f3c5226fe
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-11-15 15:11:06 +0000

    Update bot-ignore.js

--- src/general/welcome/bot-ignore.js
+++ src/general/welcome/bot-ignore.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('message', message => {
   if(message.author.bot) return; // А вот и сама проверка на бота.
   if (message.content === 'ping') {
     message.reply('Pong!');
