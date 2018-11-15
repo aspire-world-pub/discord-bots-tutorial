9b4b79e7c09bc10fbd0c3482d1fcbb6a76d49d06
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-11-15 14:53:09 +0000

    Create unban.js

---
+++ src/examples/unban.js
@@ -0,0 +1,10 @@
+let discord = require('discord.js');
+
+let member = args[0];
+if(!member) return message.channel.send("Нужно указать id пользователя");
+if(!member.bannable) return message.channel.send("Я не могу его забанить");
+await message.guild.unban(member).then(member => {
+      .catch(error => message.channel.send(`**${error}**`));
+      
+      message.channel.send(`${member.user.tag} был разбанен пользователем ${message.author.tag}`);
+      })
