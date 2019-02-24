efd87cc13f1f13c8a7ba5746923afb6e350a25d5
mishakma <45147490+mishakma@users.noreply.github.com>
2019-02-24 14:17:57 +0500

    Update unban.js

--- src/examples/unban.js
+++ src/examples/unban.js
@@ -2,7 +2,7 @@ let discord = require('discord.js');
 
 let member = args[0];
 if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('У вас нету прав на выполнение этой команды'); // проверка на права
-if(!member) return message.channel.send("Нужно упомянуть пользователя"); // если пользователь не указан
+if(!member) return message.channel.send("Нужно указать ID пользователя"); // если пользователь не указан
 if(!member.bannable) return message.channel.send('Я не могу его разбанить');
 await message.guild.unban(member).then(member => {
       
